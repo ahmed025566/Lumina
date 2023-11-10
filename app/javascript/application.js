d7b9461dc7e32d971a9b9ce6d6ce67ea0aa9b8a2
ahmed025566 <ahmedsalah025566@gmail.com>
2023-11-10 03:29:46 +0200

    Setup devise and add nav bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const adjustNav = () => {
+
+const menu =  document.getElementById('navbarNavDarkDropdown')
+  if(!menu.classList.contains('show')) {
+    menu.style.maxWidth = '36%'
+    menu.style.marginRight = '-1%'
+  } 
+}
+document.addEventListener('DOMContentLoaded', adjustNav)
